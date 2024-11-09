c1512826bb3eb7cd3c546a5d216416161fb78444
onandon <047633597q@gmail.com>
2024-11-09 20:40:08 +0800

    add: country card pagination

--- feature/country/components/country-table.tsx
+++ feature/country/components/country-table.tsx
@@ -1,54 +1,59 @@
 "use client"
 
+import { Button } from '@/components/ui/button';
+import useDebounce from '@/hooks/use-debounce';
+import Link from 'next/link';
+import { useParams, usePathname, useSearchParams } from 'next/navigation';
+import { parseAsInteger, useQueryState } from 'nuqs';
 import { useGetAllCountry } from '../api/use-get-all-country';
 import { useGetCountry } from '../api/use-get-country';
-import { useSearchParams } from 'next/navigation';
 import CountryCard from './country-card';
-import useDebounce from '@/hooks/use-debounce';
+import PaginationBar from './pagination-bar';
 
 export default function CountryTable() {
+  const FIELDSQUERY = "name,capital,population,region,flags";
+  const PERPAGE = 12;
+
   const searchParams = useSearchParams()
   const name = searchParams.get('name')
   const region = searchParams.get('region')
-  const FIELDSQUERY = "name,capital,population,region,flags";
-  const isSearched = !!name;
+  const pageIdParam = useParams().pageId as string;
+  const page = pageIdParam ? parseInt(pageIdParam, 10) : 1;
 
+  const queryParams = Object.fromEntries(searchParams.entries());
+
+  const isSearched = !!name;
   const debouncedName = useDebounce(name, 1000);
 
   const { data: allCountryQuery } = useGetAllCountry(FIELDSQUERY, !isSearched);
   const { data: countryQuery } = useGetCountry(debouncedName || "", FIELDSQUERY, isSearched);
 
-  let filteredCountry;
-
+  let filteredCountry: CountryType[] | undefined;
   if (!debouncedName) {
     filteredCountry = allCountryQuery?.data;
-    if (region && filteredCountry) {
-      filteredCountry = filteredCountry.filter((country) => country.region === region);
-    }
+  } else {
+    filteredCountry = countryQuery?.data;
+  }
 
-    return (
+  if (region && filteredCountry) {
+    filteredCountry = filteredCountry.filter((country) => country.region === region);
+  }
+
+  const totalItems = filteredCountry?.length || 0;
+  const totalPages = Math.ceil(totalItems / PERPAGE);
+  const paginatedData = filteredCountry?.slice((page - 1) * PERPAGE, page * PERPAGE);
+
+  return (
+    <div>
       <div className="grid grid-cols-4 gap-8">
-        {filteredCountry?.map((data, index) => {
+        {paginatedData?.map((data, index) => {
           return (
             <CountryCard key={index} name={data.name.official} image={data.flags.svg} region={data.region} capital={data.capital} population={data.population} />
           )
         })}
       </div>
-    )
-  }
 
-  filteredCountry = countryQuery?.data;
-  if (region && filteredCountry) {
-    filteredCountry = filteredCountry.filter((country) => country.region === region);
-  }
-
-  return (
-    <div className="grid grid-cols-4 gap-8">
-      {filteredCountry?.map((data, index) => {
-        return (
-          <CountryCard key={index} name={data.name.official} image={data.flags.svg} region={data.region} capital={data.capital} population={data.population} />
-        )
-      })}
+      <PaginationBar currentPage={page} totalPages={totalPages} queryParams={queryParams} />
     </div>
   )
 }
---
+++ feature/country/components/pagination-bar.tsx
@@ -0,0 +1,120 @@
+"use client"
+
+import React from 'react'
+import { Pagination, PaginationContent, PaginationEllipsis, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from '@/components/ui/pagination';
+import { ParsedUrlQueryInput } from 'querystring';
+
+interface PaginationBarProps {
+  currentPage: number;
+  totalPages: number;
+  queryParams: ParsedUrlQueryInput;
+};
+
+export default function PaginationBar({ currentPage, totalPages, queryParams }: PaginationBarProps) {
+  const handleNavigation = (page: number) => {
+    return {
+      pathname: `/page/${page}`,
+      query: queryParams
+    };
+  };
+
+  const toPrevPage = (page: number) => {
+    if (page === 0) {
+      return handleNavigation(page);
+    } else {
+      return handleNavigation(page - 1);
+    }
+  }
+
+  const toNextPage = (page: number) => {
+    if (page === totalPages) {
+      return handleNavigation(page);
+    } else {
+      return handleNavigation(page + 1);
+    }
+  }
+
+  if (totalPages === 0) {
+    return null;
+  }
+
+  if (totalPages <= 6) {
+    return (
+      <Pagination className="text-darkText font-semibold p-6">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious size="lg" href={toPrevPage(currentPage)} />
+          </PaginationItem>
+
+          {Array.from({ length: totalPages }).map((_, i) => (
+            <PaginationItem key={i + 1}>
+              <PaginationLink className="text-lg" href={handleNavigation(i + 1)}> {i + 1} </PaginationLink>
+            </PaginationItem>
+          ))}
+
+          <PaginationItem>
+            <PaginationNext size="lg" href={toNextPage(currentPage)} />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    )
+  }
+
+  return (
+    <Pagination className="text-darkText font-semibold p-6">
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious size="lg" href={toPrevPage(currentPage)} />
+        </PaginationItem>
+
+        <PaginationItem>
+          <PaginationLink className="text-lg" href={handleNavigation(1)}> 1 </PaginationLink>
+        </PaginationItem>
+
+        {
+          currentPage > 2 &&
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+        }
+
+        {
+          currentPage > 2 &&
+          <PaginationItem>
+            <PaginationLink className="text-lg" href={handleNavigation(currentPage - 1)}> {currentPage - 1} </PaginationLink>
+          </PaginationItem>
+        }
+
+        {
+          currentPage > 1 && currentPage < totalPages &&
+          <PaginationItem>
+            <PaginationLink className="text-lg" href={handleNavigation(currentPage)}> {currentPage} </PaginationLink>
+          </PaginationItem>
+        }
+
+        {
+          currentPage < totalPages - 1 &&
+          <PaginationItem>
+            <PaginationLink className="text-lg" href={handleNavigation(currentPage + 1)}> {currentPage + 1} </PaginationLink>
+          </PaginationItem>
+        }
+
+        {
+          currentPage < totalPages - 1 &&
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+        }
+
+        <PaginationItem>
+          <PaginationLink className="text-lg" href={handleNavigation(totalPages)}> {totalPages} </PaginationLink>
+        </PaginationItem>
+
+
+        <PaginationItem>
+          <PaginationNext size="lg" href={toNextPage(currentPage)} />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  )
+}
