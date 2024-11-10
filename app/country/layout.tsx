5e15df103b68f47f92aa3266e2221da2e3e0c492
onandon <047633597q@gmail.com>
2024-11-10 20:07:49 +0800

    add: go back button

--- app/country/layout.tsx
+++ app/country/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { FaArrowLeft } from "react-icons/fa";
 
 export default function CountryDetailLayout({
@@ -7,10 +9,13 @@ export default function CountryDetailLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+
   return (
     <div className="size-full flex flex-col">
       <div className="w-full p-7">
         <Button
+          onClick={() => router.back()}
           type="button"
           variant="outline"
           size="lg"
